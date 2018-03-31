1bf25ea6f702be39d4c986805fdaddea591a8fdb
JeremyHe <jeremyhe.cn@gmail.com>
2018-03-31 15:01:23 +0800

    2018年 3月31日 星期六 15时01分23秒 CST

---
+++ src/main/java/me/alzz/base/BaseFragment.kt
@@ -0,0 +1,20 @@
+package me.alzz.base
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.toolbar_title.*
+
+/**
+ * TODO: Add comments
+ * Created by JeremyHe on 2018/3/31.
+ */
+open class BaseFragment: Fragment() {
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        toolbar?.let {
+            (activity as? AppCompatActivity)?.setSupportActionBar(toolbar)
+        }
+    }
+}
