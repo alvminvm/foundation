6b676a7c4c31b6a28e01a723e897161d8873b6c0
JeremyHe <jeremyhe.cn@gmail.com>
2018-04-13 16:00:17 +0800

    增加 ViewExt

---
+++ src/main/java/me/alzz/base/ViewExt.kt
@@ -0,0 +1,43 @@
+package me.alzz.base
+
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.drawable.Drawable
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.widget.ImageView
+
+/**
+ * 对 view 的一些工具方法
+ * Created by JeremyHe on 2018/4/13.
+ */
+fun View.useBtnEffect() {
+    if (!this.isClickable) {
+        Log.w("ViewExt", "${this} 必须为 clickable，否则点击效果异常")
+    }
+
+    this.setOnTouchListener { v, event ->
+        var d: Drawable? = null
+        if (v is ImageView) {
+            d = v.drawable
+        }
+
+        if (d == null) {
+            d = v.background
+        }
+
+        d ?: return@setOnTouchListener false
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                d.setColorFilter(Color.LTGRAY, PorterDuff.Mode.MULTIPLY)
+            }
+            MotionEvent.ACTION_CANCEL,
+            MotionEvent.ACTION_UP -> {
+                d.clearColorFilter()
+            }
+        }
+        false
+    }
+}
