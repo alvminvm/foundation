0822cfcb825170a66cd1b86e6280fce1dee94f02
JeremyHe <jeremyhe.cn@gmail.com>
2018-05-02 20:20:19 +0800

    findTbUrl

--- src/main/java/me/alzz/base/StringExt.kt
+++ src/main/java/me/alzz/base/StringExt.kt
@@ -10,7 +10,7 @@ import android.util.Log
 /**
  * 判断字符串是否为空
  */
-fun String.isNotEmpty() = !this.isNullOrEmpty()
+fun String?.isNotEmpty() = !this.isNullOrEmpty()
 
 /**
  * 移除小数点后的0
@@ -44,6 +44,18 @@ fun CharSequence.findTbItemId(): String? {
         regex = """[?&]id=(\d{9,})""".toRegex()
         regex.find(this)?.let {
             Log.d("StringExt", "found tb id")
+            return it.groupValues[1]
+        }
+    }
+
+    return null
+}
+
+fun CharSequence.findTbShortUrl(): String? {
+    if (this.contains("tb.cn")) {
+        val regex = """(http://m\.tb\.cn/\w+) """.toRegex()
+        regex.find(this)?.let {
+            Log.d("StringExt", "found tb short url id")
             return it.groupValues[1]
         }
     }
