18f8e55865e0df2e9e4358065a9e50f8a762fcae
JeremyHe <jeremyhe.cn@gmail.com>
2019-04-07 23:18:29 +0800

    分割线

---
+++ src/main/java/me/alzz/widget/DividerDecoration.kt
@@ -0,0 +1,35 @@
+package me.alzz.widget
+
+import android.graphics.Rect
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
+import android.view.View
+
+
+/**
+ * Created by JeremyHe on 2019/4/7.
+ */
+class DividerDecoration(val width: Int): RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State?) {
+        super.getItemOffsets(outRect, view, parent, state)
+        val layoutMgr = parent.layoutManager
+        when (layoutMgr) {
+            is StaggeredGridLayoutManager -> staggerDivider(outRect, view, layoutMgr, parent)
+        }
+    }
+
+    private fun staggerDivider(outRect: Rect, view: View, layoutManager: StaggeredGridLayoutManager, parent: RecyclerView) {
+        val lp = view.layoutParams as StaggeredGridLayoutManager.LayoutParams
+        val spanCount = layoutManager.spanCount
+        val position = parent.getChildAdapterPosition(view)
+        val halfWidth = width / 2
+        outRect.left = halfWidth
+        outRect.right = halfWidth
+        outRect.top = if (position < spanCount) 0 else width
+
+        when (lp.spanIndex) {
+            0 -> outRect.left = width
+            (spanCount - 1) -> outRect.right = width
+        }
+    }
+}
