de610c00ead4adcfc40060c26d68788872133edd
JeremyHe <jeremyhe.cn@gmail.com>
2019-04-08 22:42:00 +0800

    标题支持设置方向

--- src/main/java/me/alzz/base/BaseActivity.kt
+++ src/main/java/me/alzz/base/BaseActivity.kt
@@ -1,12 +1,13 @@
 package me.alzz.base
 
 import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
 import kotlinx.android.synthetic.main.toolbar_title.*
 
 /**
  * Created by JeremyHe on 2018/3/25.
  */
-open class BaseActivity: AppCompatActivity() {
+open class BaseActivity : AppCompatActivity() {
 
     override fun setContentView(layoutResID: Int) {
         super.setContentView(layoutResID)
@@ -29,4 +30,9 @@ open class BaseActivity: AppCompatActivity() {
     override fun setTitle(titleId: Int) {
         title = getString(titleId)
     }
+
+    fun setTitleGravity(gravity: Int) {
+        titleTv.gravity = gravity
+    }
+
 }
