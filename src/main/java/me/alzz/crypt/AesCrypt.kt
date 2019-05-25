4f62cf6316bcc7403d7088114562f9e9e957304c
JeremyHe <jeremyhe.cn@gmail.com>
2019-05-25 12:09:23 +0800

    加入 aes 工具

---
+++ src/main/java/me/alzz/crypt/AesCrypt.kt
@@ -0,0 +1,30 @@
+package me.alzz.crypt
+
+import android.util.Base64
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
+
+class AesCrypt {
+    companion object {
+        //加密
+        fun encrypt(input: String, pwd: String): String {
+            val cipher = Cipher.getInstance("AES")
+            val keySpec = SecretKeySpec(pwd.toByteArray(), "AES")
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec)
+            //加密、解密
+            val encrypt = cipher.doFinal(input.toByteArray())
+            return Base64.encode(encrypt, Base64.DEFAULT).toString(Charsets.UTF_8)
+        }
+
+        //解密
+        fun decrypt(input: String, pwd: String): String {
+            val cipher = Cipher.getInstance("AES")
+            val keySpec = SecretKeySpec(pwd.toByteArray(), "AES")
+            cipher.init(Cipher.DECRYPT_MODE, keySpec)
+            //加密、解密
+            val encrypt = cipher.doFinal(Base64.decode(input, Base64.DEFAULT))
+            return encrypt.toString(Charsets.UTF_8)
+        }
+    }
+}
+
