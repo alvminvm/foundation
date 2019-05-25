f0ec3e98279579459bffd2abcfc117c8e6b46c65
JeremyHe <jeremyhe.cn@gmail.com>
2019-05-25 10:26:24 +0800

    fix progress loss state

--- src/main/java/me/alzz/Progress.kt
+++ src/main/java/me/alzz/Progress.kt
@@ -3,11 +3,7 @@ package me.alzz
 import android.app.Activity
 import android.arch.lifecycle.GenericLifecycleObserver
 import android.arch.lifecycle.Lifecycle
-import android.arch.lifecycle.LifecycleOwner
 import android.support.v7.app.AppCompatActivity
-import android.view.View
-import android.widget.TextView
-import me.alzz.base.R
 
 class Progress {
     companion object {
@@ -17,14 +13,16 @@ class Progress {
         fun show(activity: AppCompatActivity?, message: String) {
             activity ?: return
 
-            val dialog = progressMap[activity] ?: ProgressFragment().apply {
-                progressMap[activity] = this
-                activity.lifecycle.addObserver(GenericLifecycleObserver { _, event ->
-                    if (event == Lifecycle.Event.ON_DESTROY) {
-                        progressMap[activity]?.dismiss()
-                        progressMap.remove(activity)
-                    }
-                })
+            val dialog = progressMap[activity]
+                ?: activity.supportFragmentManager.findFragmentByTag(TAG) as? ProgressFragment
+                ?: ProgressFragment().apply {
+                    progressMap[activity] = this
+                    activity.lifecycle.addObserver(GenericLifecycleObserver { _, event ->
+                        if (event == Lifecycle.Event.ON_DESTROY) {
+                            progressMap[activity]?.dismissAllowingStateLoss()
+                            progressMap.remove(activity)
+                        }
+                    })
             }
 
             dialog.setMessage(message)
@@ -34,7 +32,7 @@ class Progress {
         fun dismiss(activity: Activity?) {
             activity ?: return
 
-            progressMap[activity]?.dismiss()
+            progressMap[activity]?.dismissAllowingStateLoss()
         }
     }
 }
