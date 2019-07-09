f6a7552ae5a999998060e818e3c2bc57d3ff6ced
JeremyHe <jeremyhe.cn@gmail.com>
2019-07-09 23:12:24 +0800

    move keystore helper

--- src/main/java/me/alzz/widget/MarqueeTextView.kt
+++ src/main/java/me/alzz/widget/MarqueeTextView.kt
@@ -1,15 +1,15 @@
 package me.alzz.widget
 
 import android.content.Context
+import android.support.v7.widget.AppCompatTextView
 import android.util.AttributeSet
-import android.widget.TextView
 
 /**
  * 用于实现跑马灯效果
  * Created by JeremyHe on 2018/5/3.
  */
 
-class MarqueeTextView : TextView {
+class MarqueeTextView : AppCompatTextView {
     constructor(context: Context) : super(context)
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
