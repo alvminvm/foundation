c4fb6929d24677691a28c8776e8dfdc7c4695b64
JeremyHe <jeremyhe.cn@gmail.com>
2019-07-13 20:30:14 +0800

    增加抓包证书检验

---
+++ src/main/java/me/alzz/okhttp/SnifferVerifier.kt
@@ -0,0 +1,43 @@
+package me.alzz.okhttp
+
+
+import okhttp3.internal.tls.OkHostnameVerifier
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.SSLSession
+
+/**
+ * Created by jerry on 2018/8/1.
+ */
+
+object SnifferVerifier: HostnameVerifier {
+
+    private val snifferHostnames = arrayOf(
+        "fiddler.com",
+        "fiddler2.com",
+        "charlesproxy.com",
+        "wproxy.org",
+        "Packet Capture CA Certificate",
+        "mitmproxy",
+        "Debug Proxy",
+        "HttpCanary"
+    )
+
+    override fun verify(hostname: String, session: SSLSession): Boolean {
+        for (cert in session.peerCertificateChain) {
+
+            val name = cert.issuerDN?.name ?: continue
+            if (name.isEmpty()) { continue }
+
+            for (sniffer in snifferHostnames) {
+
+                //证书信息有抓包工具的证书信息
+                if (name.contains(sniffer, true)) {
+                    return false
+                }
+            }
+
+        }
+
+        return OkHostnameVerifier.INSTANCE.verify(hostname, session)
+    }
+}
