6edd133d2d084a4ff761b51b6714b5734fcf7554
JeremyHe <jeremyhe.cn@gmail.com>
2019-07-20 14:33:07 +0800

    vm by activity

--- src/main/java/me/alzz/base/mvvm/MvvmActivity.kt
+++ src/main/java/me/alzz/base/mvvm/MvvmActivity.kt
@@ -2,23 +2,33 @@ package me.alzz.base.mvvm
 
 import me.alzz.Progress
 import me.alzz.base.BaseActivity
+import me.alzz.ext.get
 import me.alzz.ext.use
 import org.jetbrains.anko.toast
+import kotlin.reflect.KClass
 
 /**
  * Created by JeremyHe on 2019-05-26.
  */
 open class MvvmActivity: BaseActivity() {
 
-    protected fun subscribeBase(vm: BaseVM) {
-        vm.progress.use(this) {
+    private fun BaseVM.subscribeBase() {
+        this.progress.use(this@MvvmActivity) {
             if (it.isNullOrEmpty()) {
-                Progress.dismiss(this)
+                Progress.dismiss(this@MvvmActivity)
             } else {
-                Progress.show(this, it)
+                Progress.show(this@MvvmActivity, it)
             }
         }
-        vm.desc.use(this) { toast(it) }
-        vm.error.use(this) { toast(it) }
+        this.desc.use(this@MvvmActivity) { toast(it) }
+        this.error.use(this@MvvmActivity) { toast(it) }
+    }
+
+    fun <T : BaseVM> activity(vmClazz: KClass<T>): Lazy<T> {
+        return lazy {
+            val vm = get(vmClazz.java)
+            vm.subscribeBase()
+            vm
+        }
     }
 }
