3efbb1a9c88dd41bec93e5770794d41eec844865
JeremyHe <jeremyhe.cn@gmail.com>
2019-07-29 00:03:31 +0800

    add fragment page adapter

---
+++ src/main/java/me/alzz/FragmentPagerAdapter.kt
@@ -0,0 +1,18 @@
+package me.alzz
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.view.ViewGroup
+
+class FragmentPagerAdapter(fm: FragmentManager, val fragments: Array<Fragment>) : FragmentPagerAdapter(fm) {
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val f = super.instantiateItem(container, position)
+        if (f is Fragment) { fragments[position] = f }
+        return f
+    }
+
+    override fun getItem(position: Int) = fragments[position]
+    override fun getCount() = fragments.size
+}
