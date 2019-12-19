e299d981f75f3db176316e67f9b55daabc655099
JeremyHe <jeremyhe.cn@gmail.com>
2019-12-19 23:36:08 +0800

    Double.trimZero 保留两位小数

--- src/main/java/me/alzz/ext/Double.kt
+++ src/main/java/me/alzz/ext/Double.kt
@@ -3,6 +3,4 @@ package me.alzz.ext
 /**
  * Created by JeremyHe on 2019-07-27.
  */
-fun Double.trimZero(): String {
-    return this.toString().trimZero()
-}
+fun Double.trimZero() = String.format("%.2f", this).trimZero()
