7266b937459b4d883117dc63f25a6b52fc6267b8
JeremyHe <jeremyhe.cn@gmail.com>
2020-06-07 18:24:25 +0800

    RxJava to Coroutine

--- src/main/java/me/alzz/ext/Coroutine.kt
+++ src/main/java/me/alzz/ext/Coroutine.kt
@@ -1,9 +1,27 @@
 package me.alzz.ext
 
+import io.reactivex.Observable
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 /**
  * Created by JeremyHe on 2020-05-31.
  */
 suspend fun <T> withIo(block: () -> T) = withContext(Dispatchers.IO) { block() }
+
+/**
+ * Observable 转协程运行
+ */
+suspend fun <T> Observable<T>.await() = suspendCoroutine<T> {
+    this
+        .applySchedulers()
+        .subscribe({ result ->
+            it.resume(result)
+        }) { t ->
+            it.resumeWithException(t)
+        }
+}
+
