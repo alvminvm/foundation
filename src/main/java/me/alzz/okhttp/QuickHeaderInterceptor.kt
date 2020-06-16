0dc878ce3ff6deab4ac75ba07c5e2d37c793d30c
JeremyHe <jeremyhe.cn@gmail.com>
2020-06-16 23:57:02 +0800

    优化 QuickHeaderInterceptor

--- src/main/java/me/alzz/okhttp/QuickHeaderInterceptor.kt
+++ src/main/java/me/alzz/okhttp/QuickHeaderInterceptor.kt
@@ -7,20 +7,61 @@ import okhttp3.Response
 /**
  * Created by JeremyHe on 2020-05-19.
  */
-class QuickHeaderInterceptor(private val replace: (name: String, request: Request) -> String): Interceptor {
+class QuickHeaderInterceptor : Interceptor {
+
+    private val ops = mutableListOf<Any>()
+
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request()
+        var request = chain.request()
         val headers = request.headers
-        val builder = request.newBuilder()
+        var builder = request.newBuilder()
         for (i in 0 until headers.size) {
             val name = headers.name(i)
-            if (headers.value(i) == VALUE_STUB) {
-                builder.header(name, replace(name, request))
+            if (headers.value(i) != VALUE_STUB) continue
+
+            for (op in ops) {
+                if (op is AddOp) {
+                    if (!op.predicate(name)) continue
+                    val map = op.op(name, request)
+                    map.forEach { builder.header(it.key, it.value) }
+
+                    request = builder.build()
+                    builder = request.newBuilder()
+                } else if (op is ReplaceOp) {
+                    if (!op.predicate(name)) continue
+                    val value = op.op(name, request)
+                    if (value.isEmpty()) continue
+
+                    builder.header(name, value)
+
+                    request = builder.build()
+                    builder = request.newBuilder()
+                }
             }
         }
 
         return chain.proceed(builder.build())
     }
+
+    fun replace(predicate: (name: String) -> Boolean, op: (name: String, request: Request) -> String): QuickHeaderInterceptor {
+        ops.add(ReplaceOp(predicate, op))
+        return this
+    }
+
+    fun add(predicate: (name: String) -> Boolean, op: (name: String, request: Request) -> Map<String, String>): QuickHeaderInterceptor {
+        ops.add(AddOp(predicate, op))
+        return this
+    }
+
+    class ReplaceOp(
+        val predicate: (name: String) -> Boolean,
+        val op: (name: String, request: Request) -> String
+    )
+
+    class AddOp(
+        val predicate: (name: String) -> Boolean,
+        val op: (name: String, request: Request) -> Map<String, String>
+    )
 
     companion object {
         const val VALUE_STUB = "<Header Stub>"
