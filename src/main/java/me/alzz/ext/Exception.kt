7b1b0ebe718eaebd95105bdfdf7388473424d842
JeremyHe <alvminvm@qq.com>
2020-10-25 21:48:42 +0800

    feat: tryIgnore

--- src/main/java/me/alzz/ext/Exception.kt
+++ src/main/java/me/alzz/ext/Exception.kt
@@ -1,5 +1,7 @@
 package me.alzz.ext
 
+import android.util.Log
+
 /**
  * Created by JeremyHe on 2020-07-04.
  */
@@ -8,10 +10,21 @@ package me.alzz.ext
  * 捕获异常但忽略具体异常
  * @return 抛异常时返回 null
  */
-inline fun <T> nullIfException(block: () -> T): T? {
+inline fun <T> tryElseNull(block: () -> T): T? {
     return try {
         block()
     } catch (e: Exception) {
         null
+    }
+}
+
+/**
+ * 捕获异常但忽略具体异常
+ */
+inline fun <T> tryIgnore(block: () -> T) {
+    try {
+        block()
+    } catch (e: Exception) {
+        Log.w("ExceptionUtils", "ignore exception", e)
     }
 }
