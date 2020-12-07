942375ab23ecaac719fb071f806d238129c4e795
JeremyHe <alvminvm@qq.com>
2020-12-07 23:04:25 +0800

    feat: EditText bindTo & alwaysReplace

--- src/main/java/me/alzz/base/mvvm/MVVM.kt
+++ src/main/java/me/alzz/base/mvvm/MVVM.kt
@@ -1,10 +1,15 @@
 package me.alzz.base.mvvm
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import me.alzz.Progress
 import me.alzz.ext.get
@@ -12,6 +17,7 @@ import me.alzz.ext.use
 import org.jetbrains.anko.toast
 import java.lang.IllegalArgumentException
 import kotlin.reflect.KClass
+import kotlin.reflect.KMutableProperty1
 
 /**
  * Created by JeremyHe on 2019-11-16.
@@ -53,4 +59,60 @@ fun <T : BaseVM> Fragment.fragment(vmClazz: KClass<T>): Lazy<T> {
         bind(vm)
         vm
     }
+}
+
+/**
+ * 双向绑定 [EditText] 和 [LiveData]
+ * 要求 [EditText] 的宿主必须为 [LifecycleOwner]
+ * @param setter EditText set to data [T]
+ * @param getter get from data [T]
+ */
+fun <T, R> EditText.bindTo(data: LiveData<T>,
+                           setter: (receiver: T, value: R) -> Unit,
+                           getter: (receiver: T) -> R) {
+    // EditText -> LiveData
+    this.addTextChangedListener(object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val r = data.value ?: return
+            val str = s?.toString()?.trim() ?: return
+            val cur = getter(r)
+            val value = when (cur) {
+                is String -> str
+                is Int -> str.toIntOrNull() ?: return
+                is Long -> str.toLongOrNull() ?: return
+                is List<*> -> str.split(",").toList()
+                else -> throw UnsupportedOperationException("bind to 操作未支持此类型")
+            } as R
+
+            if (value == cur) return
+            setter(r, value)
+        }
+
+        override fun afterTextChanged(s: Editable?) {}
+    })
+
+    // LiveData -> EditText
+    val owner = this.context as LifecycleOwner
+    data.observe(owner, Observer {
+        val str = when (val value = getter(it)) {
+            is List<*> -> value.joinToString(",")
+            else -> value.toString()
+        }
+
+        if (text.toString() == str) return@Observer
+        this.setText(str)
+    })
+}
+
+/**
+ * 双向绑定 [EditText] 和 [LiveData]
+ * 要求 [EditText] 的宿主必须为 [LifecycleOwner]
+ * @param accessor 要绑定的具体某个属性
+ */
+fun <T, R> EditText.bindTo(data: LiveData<T>, accessor: KMutableProperty1<T, R>) {
+    bindTo(
+        data,
+        { receiver: T, value: R -> accessor.set(receiver, value) },
+        { accessor.get(it) })
 }
--- src/main/java/me/alzz/ext/EditText.kt
+++ src/main/java/me/alzz/ext/EditText.kt
@@ -1,11 +1,53 @@
 package me.alzz.ext
 
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.EditText
+import android.widget.TextView
 
 /**
  * Created by JeremyHe on 2019-11-16.
  */
 
+/**
+ * 移动光标至末尾
+ */
 fun EditText.toEnd() {
     this.setSelection(this.length())
+}
+
+/**
+ * 总是保持英文符号
+ * todo: 未完善。
+ */
+fun EditText.alwaysEnSymbol(): EditText {
+    return this.alwaysReplace("，", ",")
+}
+
+/**
+ * 总是替换 [old] 为 [new]
+ */
+fun EditText.alwaysReplace(old: String, new: String): EditText {
+    addTextChangedListener(object : TextWatcher {
+        private var postSelection = Int.MIN_VALUE
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            if (postSelection != Int.MIN_VALUE) {
+                setSelection(postSelection)
+                postSelection = Int.MIN_VALUE
+            }
+
+            val str = s?.toString() ?: return
+            val text = str.replace(old, new)
+            if (text == str) return
+
+            postSelection = maxOf(0, minOf(selectionStart + (text.length - str.length), text.length))
+            setText(text, TextView.BufferType.EDITABLE)
+        }
+
+        override fun afterTextChanged(s: Editable?) {}
+    })
+
+    return this
 }
