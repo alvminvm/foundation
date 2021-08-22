46b1d49e1e1bd11798a46212532eca9eea7323c1
AL <alvminvm@qq.com>
2021-08-22 19:38:52 +0800

    feat: using ids in FragmentPageAdapter

--- src/main/java/me/alzz/FragmentPagerAdapter.kt
+++ src/main/java/me/alzz/FragmentPagerAdapter.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import me.alzz.base.BaseFragment
 
-class FragmentPagerAdapter(fm: FragmentManager, val fragments: Array<Fragment>) : androidx.fragment.app.FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+class FragmentPagerAdapter(fm: FragmentManager, val fragments: Array<Fragment>, val ids: Array<Long>? = null) : androidx.fragment.app.FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
         val f = super.instantiateItem(container, position)
@@ -16,4 +16,7 @@ class FragmentPagerAdapter(fm: FragmentManager, val fragments: Array<Fragment>) 
     override fun getItem(position: Int) = fragments[position]
     override fun getCount() = fragments.size
     override fun getPageTitle(position: Int) = (fragments[position] as? BaseFragment)?.title
+    override fun getItemId(position: Int): Long {
+        return ids?.getOrNull(position) ?: super.getItemId(position)
+    }
 }
